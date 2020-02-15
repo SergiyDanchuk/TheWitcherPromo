f9ba1db1d0f1a3d0ce73fd421b7f36e8b1e65d9d
SergiyDanchuk <sergey.danchuk.p@gmail.com>
2020-02-15 22:09:05 +0200

    added desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
-$(function () { 
+$(function () {
 
+    var mySwiper = new Swiper('.swiper-container', {
+        speed: 400,
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        navigation: {
+            nextEl: '.swiper-button-next'
+        }
+    });
 
-    
 });
--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('sass', function () {
 gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css'
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/swiper/css/swiper.min.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -33,7 +34,8 @@ gulp.task('style', function(){
 
 gulp.task('script', function(){
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/swiper/js/swiper.min.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
