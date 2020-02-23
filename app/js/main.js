e5960c54b79b480d8190928c8450a404b2a693f1
SergiyDanchuk <sergey.danchuk.p@gmail.com>
2020-02-23 14:42:23 +0200

    added responsive design

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,20 @@
 $(function () {
 
     var mySwiper = new Swiper('.swiper-container', {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 30,
         navigation: {
             nextEl: '.swiper-button-next'
-        }
+        },
+        breakpoints: {
+            640: {
+              slidesPerView: 2,
+            },
+        },
+    });
+
+    $('.burger__menu').on('click', function () {
+        $('.header__box').slideToggle();
     });
 
 });
