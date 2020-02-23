ad17acbfd0438f7c9ece10f8b41991b622d52fba
SergiyDanchuk <sergey.danchuk.p@gmail.com>
2020-02-23 12:53:06 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,8 @@
 $(function () {
 
     var mySwiper = new Swiper('.swiper-container', {
-        speed: 400,
         slidesPerView: 2,
         spaceBetween: 30,
-        slidesPerGroup: 2,
         navigation: {
             nextEl: '.swiper-button-next'
         }
